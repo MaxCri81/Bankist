d844f58376729faa4d124124ab89eb51305ebd7f
Massimiliano Crisafulli <maxone981@gmail.com>
2025-07-10 18:32:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -59,13 +59,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+/**
+ * Clear the container of any HTML elements and forEach value in
+ * movements, create a new element, accordingly.
+ * @param {Array} movements - array of numbers (user movement figures)
+ */
 function displayMovements(movements) {
   // Clear all movement elements
   containerMovements.innerHTML = "";
   movements.forEach(createMovementElement);
   
+  /**
+   * Create an HTML element dynamically from a forEach call back.
+   * This approach can pose a risk of Cross-Site Scripting (XSS) attacks 
+   * if the HTML content comes from untrusted sources, such as user input or external data.
+   * @param {number} value - forEach array value iteration
+   * @param {number} index - forEach array index iteration
+   */
   function createMovementElementOld(value, index) {
-    // If the value is positive style and write deposit otherwise withdrawal
+    // If the value is positive style and write deposit, otherwise withdrawal
     const movementType = value > 0 ? "deposit" : "withdrawal";
     const htmlElement = `
         <div class="movements__row">
@@ -76,13 +88,20 @@ function displayMovements(movements) {
     containerMovements.insertAdjacentHTML("afterbegin", htmlElement);
   }
 
+  /**
+   * Create an HTML element dynamically from a forEach call back.
+   * This approach is safer then the insertAdjacentHTML method.
+   * @param {number} value - forEach array value iteration
+   * @param {number} index - forEach array index iteration
+   */
   function createMovementElement(value, index) {
-    // If the value is positive style and write deposit otherwise withdrawal
+    // If the value is positive style and write deposit, otherwise withdrawal
     const movementType = value > 0 ? "deposit" : "withdrawal";
 
     const row = document.createElement('div');
     row.className = 'movements__row';
-    containerMovements.append(row);
+    //containerMovements.append(row); // add at the end
+    containerMovements.insertBefore(row, containerMovements.firstChild); // insert at the beginning
 
     const movementsType = document.createElement('div');
     movementsType.classList.add('movements__type', `movements__type--${movementType}`);
@@ -95,6 +114,7 @@ function displayMovements(movements) {
     text = document.createTextNode(value);
     movementsValue.appendChild(text);
     row.append(movementsValue);
+
   }
 };
 displayMovements(account1.movements);
